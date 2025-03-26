96589ff9c1a5eeedf0aa57bbc4cd3cc59cf548ea
FabioMorrone <morronefabio04@gmail.com>
2025-03-26 18:04:42 +0100

    feat: implement task addition functionality and update rendering of task titles

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,36 @@ import { useState } from 'react'
 
 export default function App() {
   const title = ["Task1", "Task2", "Task3", "task4"]
+
+  const [titles, setTitles] = useState(title)
+
+  const addTask = e => {
+    e.preventDefault()
+    tasks.push(titles);
+
+
+  }
+
+
+
   return (
     <>
       <div className='container'>
         <div className='row'>
+          <div className='card'>
+            {
+              titles.map((item) => <div>{item}</div>)
+            }
 
+          </div>
+          <form onSubmit={addTask}>
+            <input
+              type="text"
+              value={titles}
+              onChange={e => { setTitles(e.target.value) }}
+            />
+            <button>Invia</button>
+          </form>
 
 
 
